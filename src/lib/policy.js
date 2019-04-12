e09fe6458ffcd9fe9e794942abc1f478dbaecb68
Maciej Skierkowski <maciej@skierkowski.com>
2019-04-11 19:53:45 -0700

    Improves formatting of the output

--- src/lib/policy.js
+++ src/lib/policy.js
@@ -1,7 +1,11 @@
 const color = require('chalk');
 
-const pass = color.green("PASS")
-const fail = color.red("FAIL")
+
+const stateLog = {
+  pass: color.green("passed"),
+  fail: color.red("failed"),
+  warn: color.yellow("warned")
+}
 
 const loadPolicyPlan = (policyConfig, data) => {
   return Array.from(Object.keys(policyConfig), x =>{
@@ -34,18 +38,54 @@ const loadPolicyPlan = (policyConfig, data) => {
 
 const checkPolicies = (policies) => {
   let results = []
+
+  console.log(`${color.bold("RESULTS")}---------------------\n`)
+
   for(const policy of policies) {
     let result = {policy}
     try{
-      result.pass = policy.safeguard.function(policy.data, policy.settings) === true
+      if(policy.safeguard.function(policy.data, policy.settings) === true) {
+        result.state = 'pass'
+      }
     } catch (ex) {
-      result.pass = false
+      result.state = policy.enforcement == "error" ? "fail" : "warn"
       result.message = ex.message
     }
-    results << result
+    results.push(result)
     
-    console.log(`[${result.pass ? pass : fail}] ${policy.id} ${ result.message ? `- ${result.message}` : '' }`)
+    console.log(`  ${stateLog[result.state]}: ${policy.id}`)
   }
+
+  const failedResults = results.filter(x=>x.state=="fail")
+  const warnedResults = results.filter(x=>x.state=="warn")
+
+  const totalCount = results.length
+  const failedCount = failedResults.length
+  const warnedCount = warnedResults.length
+  const passedCount = totalCount - failedCount - warnedCount
+
+  if(warnedCount > 0) {
+    console.log(`\n${color.bold("WARNINGS")}--------------------`)
+
+    warnedResults.forEach((result, i) => {
+      if(!result.pass){
+        console.log(`\n${i+1}) ${result.policy.id} ${result.message ? `\n   ${color.yellow(result.message)}`: ""}`)
+      }
+    })
+  }
+
+  if(failedCount > 0) {
+    console.log(`\n${color.bold("ERRORS")}----------------------`)
+
+    failedResults.forEach((result, i) => {
+      if(!result.pass){
+        console.log(`\n${i+1}) ${result.policy.id} ${result.message ? `\n   ${color.red(result.message)}`: ""}`)
+      }
+    })
+  }
+
+  console.log(`\n${color.bold('SUMMARY')}: ${totalCount} policies checked, ${color.red(`${failedCount} failures`)}, ${color.yellow(`${warnedCount} warnings`)}, ${color.green(`${passedCount} passed`)}`)
+
   return results
 }
 
