4a53d820ab19cc75f91e748455141eb955ba4c50
Maciej Skierkowski <maciej@skierkowski.com>
2019-04-17 23:20:07 -0700

    Adds a test case

--- src/policies/terraform/version.js
+++ src/policies/terraform/version.js
@@ -3,12 +3,7 @@ const semver = require('semver');
 module.exports = (data, settings) => {
   const { range } = settings;
   const version = data.terraform_version;
-  let passes;
-  try {
-    passes = semver.satisfies(version, range);
-  } catch (ex) {
-    throw new Error(`The version range ("${range}") is incorrectly formatted`);
-  }
+  const passes = semver.satisfies(version, range);
 
   if (!passes) {
     throw new Error(`The Terraform Version, ${version}, does not meet the required version range, ${range}`);
---
+++ test/policies/version.test.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai');
+const versionPolicy = require('../../src/policies/terraform/version');
+
+describe('terraform', () => {
+  describe('version', () => {
+    it('should pass if range requirement is met', () => {
+      const terraformState = { terraform_version: '0.12.0-beta1' };
+      const settings = { range: '^0.12.0-beta1' };
+      expect(versionPolicy(terraformState, settings)).to.be.true;
+    });
+
+    it('should fail if range requirement is not met', () => {
+      const terraformState = { terraform_version: '0.12.0' };
+      const settings = { range: '>=0.13.0' };
+      expect(() => versionPolicy(terraformState, settings)).to.throw();
+    });
+  });
+});
