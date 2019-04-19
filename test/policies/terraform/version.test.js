0b80427a78ceda17728c3ab2681756f42793fe71
Maciej Skierkowski <maciej@skierkowski.com>
2019-04-18 22:01:29 -0700

    Creating fixtures for terraform test

--- test/policies/terraform/version.test.js
+++ test/policies/terraform/version.test.js
@@ -1,18 +1,21 @@
 const { expect } = require('chai');
+const fixture = require('./fixture');
 const versionPolicy = require('../../../src/policies/terraform/version');
 
 describe('terraform', () => {
+  before(() => {
+    this.terraformState = fixture('aws-ec2');
+  });
+
   describe('version', () => {
     it('should pass if range requirement is met', () => {
-      const terraformState = { terraform_version: '0.12.0-beta1' };
       const settings = { range: '^0.12.0-beta1' };
-      expect(versionPolicy(terraformState, settings)).to.be.true;
+      expect(versionPolicy(this.terraformState, settings)).to.be.true;
     });
 
     it('should fail if range requirement is not met', () => {
-      const terraformState = { terraform_version: '0.12.0' };
       const settings = { range: '>=0.13.0' };
-      expect(() => versionPolicy(terraformState, settings)).to.throw();
+      expect(() => versionPolicy(this.terraformState, settings)).to.throw();
     });
   });
 });
