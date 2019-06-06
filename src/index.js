227dcc52eb905d5b6d807a57c479ea88a09e7ce5
Maciej Skierkowski <maciej@skierkowski.com>
2019-06-05 23:21:18 -0700

    refactor to support only base command

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-module.exports = require('@oclif/command');
+const path = require('path');
+const { Command, flags } = require('@oclif/command');
+const { loadConfig } = require('./lib/config');
+const { loadData } = require('./lib/provisioners');
+const { loadPolicyPlan, checkPolicies } = require('./lib/policy');
+
+
+class CheckCommand extends Command {
+  async run() {
+    const parsedCommand = this.parse(CheckCommand);
+    const configFile = path.resolve(process.cwd(), parsedCommand.flags.config);
+    const workingDir = path.dirname(configFile);
+
+    try {
+      const config = loadConfig(configFile);
+      const data = loadData(workingDir, { plan: parsedCommand.flags['terraform.plan'] });
+      const policies = loadPolicyPlan(config, data);
+      checkPolicies(policies);
+    } catch (ex) {
+      this.error(ex.message);
+    }
+  }
+}
+
+CheckCommand.description = `Describe the command here
+...
+Extra documentation goes here
+`;
+
+CheckCommand.flags = {
+  config: flags.string({
+    char: 'c',
+    description: 'Use a config file other than the default .safeguards.yml',
+    default: '.safeguards.yml',
+  }),
+  'terraform.plan': flags.string({
+    description: 'Specify an existing Terraform state file instead of generating a new one',
+  }),
+};
+
+module.exports = CheckCommand;
